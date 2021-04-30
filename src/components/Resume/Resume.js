3d57e5e5c4b1df343c7587a7262f630c2fa6d897
lincoln khan <mdfaysalkhancse@gmail.com>
2021-05-01 01:46:35 +0600

    portfolio website start (bootstrap)

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h1>This is resume component</h1>
+        </div>
+    );
+};
+
+export default Resume;
