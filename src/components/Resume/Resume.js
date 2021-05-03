3eca4e6997bfe3a9d2e6fc118911e7d8c84e532a
lincoln khan <mdfaysalkhancse@gmail.com>
2021-05-03 22:11:33 +0600

    code

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -7,21 +7,21 @@ const Resume = () => {
         <div class="container mt-5 mb-5">
         <div class="row">
             <div class="col-md-6 offset-md-3">
-                <h4>Latest News</h4>
+                <h4>Work History</h4>
                 <ul class="timeline">
                     <li>
-                        <a href="https://www.totoprayogo.com/">New Web Design</a>
-                        <a href="#" class="float-right">21 March, 2014</a>
+                        <a href="https://www.totoprayogo.com/"> Web Design</a>
+                        <p>21 March, 2020</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
                     </li>
                     <li>
-                        <a href="https://www.totoprayogo.com/">21 000 Job Seekers</a>
-                        <a href="https://www.totoprayogo.com/">4 March, 2014</a>
+                        <a href="https://www.totoprayogo.com/">js Training</a>
+                        <p>4 March, 2019</p>
                         <p>Curabitur purus sem, malesuada eu luctus eget, suscipit sed turpis. Nam pellentesque felis vitae justo accumsan, sed semper nisi sollicitudin...</p>
                     </li>
                     <li>
-                        <a href="https://www.totoprayogo.com/">Awesome Employers</a>
-                        <a href="https://www.totoprayogo.com/" class="float-right">1 April, 2014</a>
+                        <a href="https://www.totoprayogo.com/">React Training</a>
+                        <p>1 April, 2020</p>
                         <p>Fusce ullamcorper ligula sit amet quam accumsan aliquet. Sed nulla odio, tincidunt vitae nunc vitae, mollis pharetra velit. Sed nec tempor nibh...</p>
                     </li>
                 </ul>
--- src/components/ResumeDetails/ResumeDetails.js
+++ src/components/ResumeDetails/ResumeDetails.js
@@ -7,7 +7,7 @@ const ResumeDetails = ({resume}) => {
 			<h4>Latest News</h4>
 			<ul class="timeline">
 				<li>
-					<a href="https://www.totoprayogo.com/">{resume.title1}</a>
+					<a href="">{resume.title1}</a>
 					<a href="https://www.totoprayogo.com/" >{resume.title2}</a>
 					{/* <a href="#" className="float-right">21 March, 2014</a> */}
 					<p>{resume.description}</p>
