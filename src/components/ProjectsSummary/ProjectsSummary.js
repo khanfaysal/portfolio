bab5b137e40c18e7532fa78cb1d6957d55811206
khan faysal <mdfaysalkhancse@gmail.com>
2023-03-01 03:26:52 +0600

    comment out load more button in projects components and added new small project color generator in vanila js

--- src/components/ProjectsSummary/ProjectsSummary.js
+++ src/components/ProjectsSummary/ProjectsSummary.js
@@ -2,8 +2,7 @@ import React from "react";
 import ProjectSummaryDetails from "../ProjectsSummaryDetails/ProjectSummaryDetails";
 import "./ProjectSummary.css";
 import carService from "../../images/carService.png";
-import bookShop from "../../images/bookshop.png";
-import rideService from "../../images/rideService.png";
+import colorGenerate from "../../images/Color-Generate.png";
 import shopNyla from "../../images/Shop-Nyla.png";
 import pixel from "../../images/Pixel.png";
 import { useState } from "react";
@@ -19,50 +18,41 @@ const ProjectsSummary = () => {
       image: carService,
       title: "Car Repair Website",
       description:
-        "The user gets service, to give review also see specific order.Includes login system.This website controlled by an admin with a beautiful dashboard and any admin can make any other account admin.",
+        "The user gets service, to give review also see specific order.This website controlled by an admin with a beautiful dashboard and any admin can make any other account admin.",
       liveLink: "https://car-repair-8145b.web.app/",
       clientLink: "https://github.com/khanfaysal/car-repair-client",
-      serverLink:"https://github.com/khanfaysal/car-repair-server",
-      tags:"reactjs,express js,react-router,bootstrap5,firebase-auth,mongoDB"
+      serverLink: "https://github.com/khanfaysal/car-repair-server",
+      tags: "reactjs,express js,react-router,bootstrap5,firebase-auth,mongoDB"
     },
+
     {
-      image: rideService,
-      title: "Grab Rides Website",
+      image: colorGenerate,
+      title: "Color Generate Website",
       description:
-        "Customer booking rides from different vehicles chosen if he/she already login.Can choose pick and destination point and also see google map (iframe).Besides customer to see the vehicle availability.",
-      liveLink: "https://react-auth-assignment-89f47.web.app/",
-      clientLink: "https://github.com/khanfaysal/grab-rides",
-      tags:"reactjs,bootstrap5,react-router,firebase-auth,iframes,"
+        "This website is generate random color as a result we choose our desire color and copy or grab color with hex value. When our any projects need color then grab here. After copy hex code you can use directly in your project.",
+      liveLink: "https://zingy-manatee-7646ba.netlify.app/",
+      clientLink: "https://github.com/khanfaysal/color-generate",
+      tags: "HTML,CSS3,javascript"
     },
     {
-      image: bookShop,
-      title: "Book Region Website",
-      description:
-        "This website is one kind of book shopping website.Customer order books from home page and checkout. Admin manages books and adds books.Includes google login systems",
-      liveLink: "https://book-shop-978b6.web.app/",
-      clientLink: "https://github.com/khanfaysal/book-region",
-      serverLink:"https://github.com/khanfaysal/book-region-server",
-      tags:"bootstrap5,reactjs,react-router,firebase-auth,react-hook form"
-    },
-    {
-      image: pixel ,
+      image: pixel,
       title: "Pixel Website",
       description:
         "Pixel website is mainly focused on sharing photos, illustrations. Pixel images and videos may be used freely for commercial or non-commercial.",
       liveLink: "https://tailwind-project-d9330.web.app/",
       clientLink: "https://github.com/khanfaysal/tailwind-project",
-      tags:"tailwind css,reactjs,react-router,firebase-auth"
+      tags: "tailwind css,reactjs,react-router,firebase-auth"
     },
     {
-      image: shopNyla ,
+      image: shopNyla,
       title: "Shop Nyla Website ( Team Project )",
       description:
-        "Users can buy products after login into the applications.They can see beautiful UI design and also delete, increase/decrease product quantity while they are select to buy the products. Therefore when users select a product they can ensure which product is selected and also see the product's total price, shipping cost, vat, and total price.",
+        "Users can buy products after login into the applications.Delete, increase/decrease product quantity option exists. Therefore when users select a product they can ensure which product is selected and also see the product's total price, shipping cost, vat, and total price.",
       liveLink: "https://shop-nyla.web.app/",
       clientLink: "https://github.com/mir-hussain/shop-nyla-client/branches",
-      tags:"redux,reactjs,react-router,firebase-auth,emailjs"
+      tags: "redux,reactjs,react-router,firebase-auth,emailjs"
     },
-    
+
   ];
   return (
     <section className="section-projectSummary section">
@@ -73,9 +63,9 @@ const ProjectsSummary = () => {
             {projectsData.slice(0, visible).map((proData) => (
               <ProjectSummaryDetails proData={proData}></ProjectSummaryDetails>
             ))}
-            
+
           </div>
-          <button onClick={showMoreProjects} className="brand-btn">Load more</button>
+          {/* <button onClick={showMoreProjects} className="brand-btn">Load more</button> */}
         </div>
       </div>
     </section>
