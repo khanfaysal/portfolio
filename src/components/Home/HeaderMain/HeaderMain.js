8fec4834cf508a6b00b136252eabb697c220bef1
khan faysal <mdfaysalkhancse@gmail.com>
2023-05-28 17:52:40 +0600

    portfolio added new logo and modify home page

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../../images/testhero.png";
+import logo from "../../../images/logo-line-big.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithubAlt,
@@ -12,28 +12,20 @@ import "./HeaderMain.css";
 
 const HeaderMain = () => {
   return (
-    <main className=' main-div row '>
-      <div className='col-md-12 d-flex justify-content-center align-items-center pt-5'>
-        <div className='col-md-4'>
-          {" "}
-          <img src={logo} alt='' className='my-logo img-fluid rounded-circle' />
-        </div>
+    <main className='main-div row'>
+      <div className='col-md-4 d-flex justify-content-center align-items-end'>
+        <img src={logo} alt='' className='my-logo img-fluid' />
+      </div>
 
-        <div className='col-md-4 name-designation-wrap '>
+      <div className='col-md-6 d-flex flex-column align-items-center justify-content-center'>
+        <div>
           <h1 className='banner-heading pt-3'>FAYSAL KHAN</h1>
-          <h3 className='name-heading pb-2'>
-            {/* <Typewriter
-            options={{
-              strings: ['React', 'Js'],
-              autoStart: true,
-              loop: true,
-            }}
-          /> */}
-            Web Developer
-          </h3>
+          <h3 className='name-heading pb-2'>Web Developer</h3>
         </div>
+      </div>
 
-        <div className='d-flex col-md-4 pt-5'>
+      <div className='col-md-2 d-flex justify-content-end align-items-center'>
+        <div className='social-part'>
           <ul>
             <li>
               <a href='https://github.com/khanfaysal'>
