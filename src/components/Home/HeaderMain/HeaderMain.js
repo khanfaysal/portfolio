d12fdd7e29c951562b2bd3804f18c0e8da287d22
khan faysal <mdfaysalkhancse@gmail.com>
2023-05-29 22:18:37 +0600

    [DONE] css changes with all color

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -4,23 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithubAlt,
   faLinkedinIn,
-  faMedium,
   faMediumM,
-  faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
-import { faDownload, faFile } from "@fortawesome/free-solid-svg-icons";
-import Typewriter from "typewriter-effect";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+// import Typewriter from "typewriter-effect";
 import "./HeaderMain.css";
 
 const HeaderMain = () => {
   return (
     <main className='main-div row'>
-      <div className='col-md-4 d-flex justify-content-center align-items-center'>
-        <img src={logo} alt='logo' className='my-logo img-fluid' />
-      </div>
 
-      <div className='col-md-6 d-flex flex-column align-items-center justify-content-center'>
-        <div>
+      <div className='name-banner-wrap col-md-10 d-flex flex-column align-items-center justify-content-center'>
+        <div className='name-banner'>
           <h1 className='banner-heading pt-3'>FAYSAL KHAN</h1>
           <h3 className='name-heading pb-2'>Web Developer</h3>
         </div>
@@ -30,22 +25,22 @@ const HeaderMain = () => {
         <div className='social-part'>
           <ul>
             <li>
-              <a href='https://github.com/khanfaysal' target='_blank'>
+              <a href='https://github.com/khanfaysal' target='_blank' rel="noreferrer">
                 <FontAwesomeIcon icon={faGithubAlt}></FontAwesomeIcon>
               </a>
             </li>
             <li>
-              <a href='https://www.linkedin.com/in/faysal-khan-47aa8710a/' target='_blank'>
+              <a href='https://www.linkedin.com/in/faysal-khan-47aa8710a/' target='_blank' rel="noreferrer">
                 <FontAwesomeIcon icon={faLinkedinIn}></FontAwesomeIcon>
               </a>
             </li>
             <li>
-              <a href='https://medium.com/@devfaysalkhan' target='_blank'>
+              <a href='https://medium.com/@devfaysalkhan' target='_blank' rel="noreferrer">
                 <FontAwesomeIcon icon={faMediumM}></FontAwesomeIcon>
               </a>
             </li>
             <li>
-              <a href='https://drive.google.com/file/d/1TYoVlHJyYofoJk_ZIE9ULfhxRVDf_p-q/view?usp=sharing' target='_blank'>
+              <a href='https://drive.google.com/file/d/1TYoVlHJyYofoJk_ZIE9ULfhxRVDf_p-q/view?usp=sharing' target='_blank' rel="noreferrer">
                 <FontAwesomeIcon icon={faFile}></FontAwesomeIcon>
               </a>
             </li>
--- src/components/ProjectsSummaryDetails/ProjectSummaryDetails.js
+++ src/components/ProjectsSummaryDetails/ProjectSummaryDetails.js
@@ -2,20 +2,20 @@ import React from "react";
 import "./ProjectSummaryDetails.css";
 
 const ProjectSummaryDetails = (props) => {
-  const {image, title, description, liveLink, clientLink, serverLink} = props.proData;
+  const { image, title, description, liveLink, clientLink, serverLink } = props.proData;
   const technologySplit = props.proData.tags.split(',')
   return (
     <div>
       <div class="card shadow p-3 mb-5 bg-body rounded">
         <img src={image} class="card-img-top" alt="..." />
         <div class="card-body">
-            <div className="mt-3">
-              {
-                technologySplit.map(tag =>(
-                  <span key={props.proData.title} class="badge rounded-pill bg-warning text-dark m-2">{tag}</span>
-                ))
-              }
-            </div>
+          <div className="mt-3">
+            {
+              technologySplit.map(tag => (
+                <span key={props.proData.title} class="badge rounded-pill bg-gray shadow-sm text-dark m-2">{tag}</span>
+              ))
+            }
+          </div>
           <h5 class="card-title pt-4">{title}</h5>
           <p class="card-text">{description}</p>
           <div className="link-part d-flex justify-content-around">
